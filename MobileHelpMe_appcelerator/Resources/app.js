b0d501e07adf74c15aa5beab16297f4e8762dd8a
Gregory Tomei <gregory@websystemstudio.com>
2011-04-16 21:38:38 -0700

    getting the first screen to a decent appearance. twitter dialog still working?

--- MobileHelpMe_appcelerator/Resources/app.js
+++ MobileHelpMe_appcelerator/Resources/app.js
@@ -1,70 +1,81 @@
-// this sets the background color of the master UIView (when there are no windows/tab groups on it)
-Titanium.UI.setBackgroundColor('#000');
-
-// create tab group
-var tabGroup = Titanium.UI.createTabGroup();
-
-
 //
 // create base UI tab and root window
 //
-var win_signin = Titanium.UI.createWindow({  
-    title:'Mobile',
-    backgroundColor:'#fff'
+var win = Titanium.UI.createWindow({
+    backgroundColor:'#63b8ff'
 });
-var label_twitter = Titanium.UI.createLabel({
-    text:"Sign with with Twitter to begin",
-    height:'auto',
-    width:'auto',
-    shadowColor:'#aaa',
-    shadowOffset:{x:5,y:5},
-    color:'#900',
+//
+// create the first screen: 3 labels and a button
+//
+var scr1_view = Ti.UI.createView();
+var scr1_label_appname = Titanium.UI.createLabel({
+    text:"PaidAid",
     font:{fontSize:48},
+    color:'white',
+    top:0,
+    width:'auto',height:'auto',
     textAlign:'center'
 });
-var button_twitter = Titanium.UI.createButton({
-    title:"Twitter oAuth..."
+var scr1_label_tagline = Titanium.UI.createLabel({
+    text:"Get help and help others",
+    color:'blue',
+    font:{fontSize:24},
+    top:80,
+    width:'auto',height:'auto',
+    textAlign:'center'
 });
-button_twitter.addEventListener('click', function() {
-    Titanium.API.info("You clicked the button");
+var scr1_label_instructions = Titanium.UI.createLabel({
+    text:"Sign-in to get started",
+    color:'white',
+    font:{fontSize:24},
+    top:160,
+    width:'auto',height:'auto',
+    textAlign:'center'
+});
+var scr1_button_twitter = Titanium.UI.createButton({
+    height:24,
+    width:151,
+    top:240,
+    backgroundImage:"sign-in-with-twitter-light.png" // dimensions of img 385x65. could use this: http://blog.140proof.com/post/569779156/scalable-sign-in-button
+});
+scr1_button_twitter.addEventListener('click', function() {
+    Titanium.API.info("GVT: button 'scr1_button_twitter' click event...");
     perform_twitter_auth();
+    // if (perform_twitter_auth()) {
+    //     // go to next screen
+    // };
 });
+scr1_view.add(scr1_label_appname, scr1_label_tagline, scr1_label_instructions, scr1_button_twitter);
 
+/**
+ * taken from: http://ziodave.tumblr.com/post/746024933/titanium-implementation-of-twitter-oauth
+ * by David Riccitelli http://twitter.com/ziodave
+ */
 function perform_twitter_auth()
 {
-    // by David Riccitelli http://twitter.com/ziodave
-
     Ti.include('oauth_adapter.js');
-
-    // create a new OAuthAdapter instance by passing by your consumer data and signature method
     var oAuthAdapter = new OAuthAdapter(
-    'qsznnVkSYoRyV9c5Hild8W2oWZuHIm9jd11cMxGY',
-    '95PnpZmcjjJOq2ys6ECXA',
-    'HMAC-SHA1'
+        'qsznnVkSYoRyV9c5Hild8W2oWZuHIm9jd11cMxGY',
+        '95PnpZmcjjJOq2ys6ECXA',
+        'HMAC-SHA1'
     );
-
     // load the access token for the service (if previously saved)
     oAuthAdapter.loadAccessToken('twitter');
-
-    // consume a service API - in this case the status update by Twitter. notice the nested array for the parameters argument
-    oAuthAdapter.send('https://api.twitter.com/1/statuses/update.json', [['status','Hey @ziodave, I managed to use the #oauth adapter for @titanium consuming @twitterapi']],'Twitter','Tweet published.','Tweet not published.');
-
-    // if the client is not authorized, ask for authorization. the previous tweet will be sent automatically after authorization
+    // if the client is not authorized, ask for authorization.
     if (oAuthAdapter.isAuthorized() == false)
     {
         // this function will be called as soon as the application is authorized
         var receivePin = function() {
-        // get the access token with the provided pin/oauth_verifier
-        oAuthAdapter.getAccessToken('https://api.twitter.com/oauth/access_token');
-        // save the access token
-        oAuthAdapter.saveAccessToken('twitter');
-    };
-
-    // show the authorization UI and call back the receive PIN function
-    oAuthAdapter.showAuthorizeUI('https://api.twitter.com/oauth/authorize?' + oAuthAdapter.getRequestToken('https://api.twitter.com/oauth/request_token'), receivePin);
+            // get the access token with the provided pin/oauth_verifier
+            oAuthAdapter.getAccessToken('https://api.twitter.com/oauth/access_token');
+            // save the access token
+            oAuthAdapter.saveAccessToken('twitter');
+        };
+        // show the authorization UI and call back the receive PIN function
+        oAuthAdapter.showAuthorizeUI('https://api.twitter.com/oauth/authorize?' + oAuthAdapter.getRequestToken('https://api.twitter.com/oauth/request_token'), receivePin);
     }
+    return true; // assume that if we got this far, it was successful
 }
 
-win_signin.add(label_twitter);
-win_signin.add(button_twitter);
-win_signin.open();
+win.add(scr1_view);
+win.open();
