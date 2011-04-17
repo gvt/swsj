525148e8542edffd0cd23b704f12c129d0fff8ea
Gregory Tomei <gregory@websystemstudio.com>
2011-04-16 23:29:48 -0700

    re-adding new rails app (not as submodule anymore)

---
+++ MobileHelpMe_rails/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MobileHelpMeRails::Application.config.secret_token = '1d16c6d5a487e3e16a11e16277d956172bf1f44739fc294780ce6ab86fc3fbd07e8e2818a8aa45b87365d96dfe60f5493ce1737a98baa27e3ea8af920c444cac'
