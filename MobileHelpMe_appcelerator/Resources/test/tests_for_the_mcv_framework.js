279eb16a1120cb35868161f882b80373facb8ae1
Gregory Tomei <gregory@websystemstudio.com>
2011-04-20 23:04:08 -0700

    comments

--- MobileHelpMe_appcelerator/Resources/test/tests_for_the_mcv_framework.js
+++ MobileHelpMe_appcelerator/Resources/test/tests_for_the_mcv_framework.js
@@ -1,11 +1,11 @@
-//
-// uncomment the lib includes below to run this test suite by itself.
-// leave the libs commented to run inconjuction with the app code tests (test/test.js)
-// if the lib files are included twice (here and in test.js) you'll get an stack too deep error.
-//
 (function(){
-    // Ti.include('test/lib/jasmine-1.0.2.js');
-    // Ti.include('test/lib/jasmine-titanium.js');
+    //
+    // leave the lib includes below uncommented to run this test suite by itself.
+    // comment them out to run inconjuction with the app code tests (test/test.js)
+    // if the lib files are included twice (here and in test.js) you'll get an stack too deep error.
+    //
+    Ti.include('test/lib/jasmine-1.0.2.js');
+    Ti.include('test/lib/jasmine-titanium.js');
 	
     // framework
     Ti.include('test/mcv-1.0-tests/core.new.js');
