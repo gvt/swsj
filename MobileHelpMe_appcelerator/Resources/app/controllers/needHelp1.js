5bbfc0910d335571647682a001a143a2ceff3768
Gregory Tomei <gregory@websystemstudio.com>
2011-04-21 23:58:20 -0700

    test and workingness

--- MobileHelpMe_appcelerator/Resources/app/controllers/needHelp1.js
+++ MobileHelpMe_appcelerator/Resources/app/controllers/needHelp1.js
@@ -1,7 +1,14 @@
 mcv.create('controller', 'needhelp1', {
 
-    next: function()
+    next: function(strLocation)
     {
+        // capture the taskRequest location
+        if (typeof strLocation === 'string' && !!App.d.taskRequest) {
+            App.d.taskRequest.attributes.location = strLocation;
+        } else {
+            Ti.API.warn("controller.needhelp1 :: next(): did not set taskRequest.location, [App.d.taskRequest="+ App.d.taskRequest +", strLocation="+ strLocation +"]")
+        }
+        // on to the next
         this.n(mcv.c.needhelp2);
     },
 });
--- MobileHelpMe_appcelerator/Resources/test/controllers/test_needHelp1.js
+++ MobileHelpMe_appcelerator/Resources/test/controllers/test_needHelp1.js
@@ -7,8 +7,7 @@
 	    beforeEach(function() {
 	        c = mcv.c.needhelp1;
             expect(c).toBeDefined();
-            expect(c).not.toBeNull
-            ();
+            expect(c).not.toBeNull();
 	    });
 
         describe('next', function() {
@@ -24,11 +23,13 @@
                 expect(c.n).toHaveBeenCalledWith(mcv.c.needhelp2);
             });
 
-            xit("captures user input", function() {
+            it("captures user input", function() {
                 spyOn(c,'n');
+                App.d.taskRequest = {attributes:{location:null}}; // mock this using literals because there are no function calls used that i can mock
 
-                var tr = {blah:"..."};
-                c.next();
+                var loc = "37.77018737792969, -122.4037094116211";
+                c.next(loc);
+                expect(App.d.taskRequest.attributes.location).toEqual(loc);
                 expect(c.n).toHaveBeenCalledWith(mcv.c.needhelp2);
             });
 
